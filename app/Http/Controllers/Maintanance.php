a89facf313f06e950c4d1279d2fa4fcd942f31d8
DMJ-Code <distrindo.multijaya@gmail.com>
2023-05-10 14:41:16 +0700

    add maintanance page

---
+++ app/Http/Controllers/Maintanance.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Maintanance extends Controller
+{
+    public function maintanance(Request $request)
+    {
+        return view('maintanance.service');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardDMJ;
 use App\Http\Controllers\DashboardDMJBTASController;
 use App\Http\Controllers\DashboardIKABDGController;
 use App\Http\Controllers\DashboardIKAController;
+use App\Http\Controllers\Maintanance;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,21 +18,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(DashboardDMJ::class)->group(function () {
-    Route::get('/dashboard_DMJ', 'dashboarddmj')->name('DMJS');
-    Route::get('/dashboard_IKA', 'dashboardika')->name('IKA');
-    Route::get('/', 'dashboarddmj')->name('DMJS');
-    Route::post('/filter', 'carimonthly')->name('filltermonthly');
-    Route::get('/filters', 'carimonthly')->name('filltermonthlyS');
-    Route::post('/tambah_target', 'createtarget')->name('Tambah Target');
-});
-Route::controller(DashboardIKAController::class)->group(function () {
-    Route::get('/dashboard_IKA', 'dashboardika')->name('IKA');
-});
-Route::controller(DashboardIKABDGController::class)->group(function () {
-    Route::get('/dashboard_IKABDG', 'dashboardikabdg')->name('IKABDG');
-});
-Route::controller(DashboardDMJBTASController::class)->group(function () {
-    Route::get('/dashboard_DMJBTA', 'dashboarddmjbta')->name('DMJBTA');
+// Route::controller(DashboardDMJ::class)->group(function () {
+//     Route::get('/dashboard_DMJ', 'dashboarddmj')->name('DMJS');
+//     Route::get('/dashboard_IKA', 'dashboardika')->name('IKA');
+//     Route::get('/', 'dashboarddmj')->name('DMJS');
+//     Route::post('/filter', 'carimonthly')->name('filltermonthly');
+//     Route::get('/filters', 'carimonthly')->name('filltermonthlyS');
+//     Route::post('/tambah_target', 'createtarget')->name('Tambah Target');
+// });
+// Route::controller(DashboardIKAController::class)->group(function () {
+//     Route::get('/dashboard_IKA', 'dashboardika')->name('IKA');
+// });
+// Route::controller(DashboardIKABDGController::class)->group(function () {
+//     Route::get('/dashboard_IKABDG', 'dashboardikabdg')->name('IKABDG');
+// });
+// Route::controller(DashboardDMJBTASController::class)->group(function () {
+//     Route::get('/dashboard_DMJBTA', 'dashboarddmjbta')->name('DMJBTA');
+// });
+Route::controller(Maintanance::class)->group(function () {
+    Route::get('/', 'maintanance')->name('services');
+    Route::get('/maintanance', 'maintanance')->name('services');
+
+    Route::get('/dashboard_DMJ', 'maintanance')->name('services');
+    Route::get('/dashboard_IKA', 'maintanance')->name('services');
+    Route::get('/', 'maintanance')->name('services');
+    Route::post('/filter', 'maintanance')->name('services');
+    Route::get('/filters', 'maintanance')->name('services');
+    Route::post('/tambah_target', 'maintanance')->name('services');
+
+    Route::get('/dashboard_IKA', 'maintanance')->name('services');
+    Route::get('/dashboard_IKABDG', 'maintanance')->name('services');
+    Route::get('/dashboard_DMJBTA', 'maintanance')->name('services');
+
+
+
 });
 
